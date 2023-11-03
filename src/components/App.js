06983c4768dc43367ebf090dbeec99e5513f4241
ravine13 <kuriaderek@gmail.com>
2023-11-03 07:41:17 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,15 @@ import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState([
+  const [movies,setMovies] = useState([
     { id: 1, title: "A River Runs Through It" },
     { id: 2, title: "Se7en" },
     { id: 3, title: "Inception" }
   ]);
+
+  function addMovie(newMovie) {
+    setMovies([...movies, newMovie]);
+  }
 
   return (
     <div>
@@ -16,6 +20,9 @@ function App() {
       <Switch>
         <Route path="/movies">
           <MoviesPage movies={movies} />
+          <button onClick={() => addMovie({ id: 4, title:"" })}>
+        Add Movie
+      </button>
         </Route>
         <Route exact path="/">
           <div>Home</div>
